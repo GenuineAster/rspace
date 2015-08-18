9ef0dfeb49365002b35ecbb18359c1a1819c08a1
Mischa Aster Alff <mischa.alff@gmail.com>
2015-08-18 16:28:28 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate graphics;
+extern crate piston_window;
+extern crate opengl_graphics;
+
+use piston::window::*;
+
+fn main() {
+	let window = PistonWindow;
+    println!("Hello, world!");
+}
+
