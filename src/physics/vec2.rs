bce8e7f95ea56019dfc2f13db439f89615009b17
Mischa Aster Alff <mischa.alff@gmail.com>
2015-08-20 18:30:45 +0200

    Generic generics

--- src/physics/vec2.rs
+++ src/physics/vec2.rs
@@ -4,29 +4,31 @@ pub struct Vec2<T> {
 	pub y : T
 }
 
-impl Vec2<f64> {
-	pub fn length2(&self) -> f64 {
+use num::traits::*;
+
+impl<N: Num + Copy> Vec2<N> {
+	pub fn length2(&self) -> N {
 		self.x * self.x + self.y * self.y
 	}
+}
 
-	pub fn length(&self) -> f64 {
+impl<N: Num + Float> Vec2<N> {
+	pub fn length(&self) -> N {
 		self.length2().sqrt()
 	}
 }
 
-use num::traits::*;
-
 use std::ops::Add;
-impl<N : Num> Add for Vec2<N> {
+impl<N: Num + Add> Add for Vec2<N> {
 	type Output = Vec2<N>;
 
 	fn add(self, rhs:Self::Output) -> Self::Output {
-		return Vec2 { x:self.x + rhs.x, y:self.y + rhs.y };
+		Vec2 { x:self.x + rhs.x, y:self.y + rhs.y }
 	}
 }
 
 use std::ops::Sub;
-impl<N : Num> Sub for Vec2<N> {
+impl<N: Num + Sub> Sub for Vec2<N> {
 	type Output = Vec2<N>;
 
 	fn sub(self, rhs:Self::Output) -> Self::Output {
@@ -35,8 +37,8 @@ impl<N : Num> Sub for Vec2<N> {
 }
 
 use std::ops::Neg;
-impl Neg for Vec2<f64> {
-	type Output = Vec2<f64>;
+impl<N: Signed + Neg> Neg for Vec2<N> {
+	type Output = Vec2<N>;
 
 	fn neg(self) -> Self::Output {
 		return Vec2 { x: -self.x, y: -self.y};
@@ -44,7 +46,7 @@ impl Neg for Vec2<f64> {
 }
 
 use std::ops::Mul;
-impl<N : Num> Mul for Vec2<N> {
+impl<N: Num + Mul> Mul for Vec2<N> {
 	type Output = Vec2<N>;
 
 	fn mul(self, rhs:Self::Output) -> Self::Output {
@@ -52,34 +54,34 @@ impl<N : Num> Mul for Vec2<N> {
 	}
 }
 
-impl Mul<f64> for Vec2<f64> {
-	type Output = Vec2<f64>;
+impl<N: Num + Mul + Copy> Mul<N> for Vec2<N> {
+	type Output = Vec2<N>;
 
-	fn mul(self, rhs:f64) -> Self::Output {
-		return Vec2 { x:self.x * rhs, y:self.y * rhs};
+	fn mul(self, rhs : N) -> Self::Output {
+		Vec2 { x: self.x * rhs, y: self.y * rhs }
 	}
 }
 
 use std::ops::Div;
-impl Div<f64> for Vec2<f64> {
-	type Output = Vec2<f64>;
+impl<N: Num + Div + Copy> Div<N> for Vec2<N> {
+	type Output = Vec2<N>;
 
-	fn div(self, rhs:f64) -> Self::Output {
+	fn div(self, rhs:N) -> Self::Output {
 		return Vec2 { x:self.x / rhs, y:self.y / rhs};
 	}
 }
 
 use std::cmp::Ordering;
 use std::cmp::PartialEq;
-impl<N : Num> PartialEq for Vec2<N> {
+impl<N: Num + PartialEq> PartialEq for Vec2<N> {
 	fn eq(&self, rhs:&Vec2<N>) -> bool {
 		self.x == rhs.x && self.y == rhs.y
 	}
 }
 
 use std::cmp::PartialOrd;
-impl PartialOrd for Vec2<f64> {
-	fn partial_cmp(&self, rhs:&Vec2<f64>) -> Option<Ordering> {
+impl<N: Num + PartialOrd> PartialOrd for Vec2<N> {
+	fn partial_cmp(&self, rhs:&Vec2<N>) -> Option<Ordering> {
 		if self.x > rhs.x && self.y > rhs.y {
 			return Some(Ordering::Greater);
 		} else if self.x < rhs.x && self.y < rhs.y {
